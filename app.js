c5a293a3c58f54919cd16aa0e69594a00c145328
Jeremiah P <speceod@gmail.com>
2025-01-20 20:49:17 -0800

    project 12 commit 3

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const mainRouter = require("./routes/index");
+const process = require("process");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -24,6 +25,3 @@ app.use("/", mainRouter);
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`);
 });
-
-
-
